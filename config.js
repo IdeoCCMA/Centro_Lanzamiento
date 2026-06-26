0bc5c0a72f1ac6d812bd4ba8443b2ecf145b148c
IdeoCCMA <esalinasf@camaramedellin.com.co>
2026-06-26 10:06:58 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuración de contraseñas por botón
+const MIS_CONTRASENAS = {
+    "cartas-nit": "00",
+    "reporte-incidencia": "1234",
+    "boletin": "abc",
+    "calidad": "999"
+    // El botón "Reporte de Libros" no se incluye aquí porque es libre
+};
