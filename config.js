861f3ee56213f9cbe8cee0425405fea78d9a7f2d
IdeoCCMA <esalinasf@camaramedellin.com.co>
2026-06-30 10:57:48 -0500

    Modify password values in config.js
    Updated password configuration values for buttons.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Configuración de contraseñas por botón
 const MIS_CONTRASENAS = {
-    "cartas-nit": "00",
+    "cartas-nit": "10",
     "reporte-incidencia": "1234",
-    "boletin": "abc",
-    "calidad": "999"
+    "boletin": "20",
+    "calidad": "30"
     // El botón "Reporte de Libros" no se incluye aquí porque es libre
 };
